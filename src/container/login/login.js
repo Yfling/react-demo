7948c97f1e9215f1107e40e9df775f1f014b5320
Yfling <598184085@qq.com>
2018-07-30 09:16:18 +0800

    add: 7-1 完善信息-boss信息完善页面

--- src/container/login/login.js
+++ src/container/login/login.js
@@ -47,7 +47,7 @@ class Login extends React.Component{
 						<WhiteSpace />
 						<InputItem
 							onChange={v=>this.handleChange('pwd',v)}
-
+							type='password'
 						>密码</InputItem>
 					</List>
 					<WhiteSpace />
