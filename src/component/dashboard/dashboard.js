4e7402a0b4276c302241e79e6271d444695bf1ae
Yfling <598184085@qq.com>
2018-08-05 17:35:38 +0800

    add: 8-3

--- src/component/dashboard/dashboard.js
+++ src/component/dashboard/dashboard.js
@@ -24,33 +24,38 @@ class Dashboard extends React.Component{
     const {pathname} = this.props.location
     const user = this.props.user
     const navList = [
-      {path: '/boss',
-        text: '牛人',
-        icon: 'boss',
-        title: '牛人列表',
-        component: Boss,
-        hide: user.type === '  '
-      },
-      {path: '/genius',
-        text: 'boss',
-        icon: 'job',
-        title: 'BOSS列表',
-        component: Genius,
-        hide: user.type === 'boss'
-      },
-      {path: '/msg',
-        text: '消息',
-        icon: 'msg',
-        title: '消息列表',
-        component: Msg,
-      },
-      {path: '/me',
-        text: '我',
-        icon: 'user',
-        title: '个人中心',
-        component: User,
-      }
-    ]
+			{
+				path:'/boss',
+				text:'牛人',
+				icon:'boss',
+				title:'牛人列表',
+				component:Boss,
+				hide:user.type=='genius'
+			},
+			{
+				path:'/genius',
+				text:'boss',
+				icon:'job',
+				title:'BOSS列表',
+				component:Genius,
+				hide:user.type=='boss'
+			},
+			{
+				path:'/msg',
+				text:'消息',
+				icon:'msg',
+				title:'消息列表',
+				component:Msg
+			},
+			{
+				path:'/me',
+				text:'我',
+				icon:'user',
+				title:'个人中心',
+				component:User
+			}
+		]
+
 
     return (
       <div>
--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import Dashboard from './component/dashboard/dashboard'
 import reducers from './reducer'
 import './config'
 import './redux/index.css'
+import './index.css'
 const store = createStore(reducers, compose(
   applyMiddleware(thunk),
   window.devToolsExtension?window.devToolsExtension():f=>f
