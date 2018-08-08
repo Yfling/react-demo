6d2396879130806b62d0593f5aba824a6ae94230
Yfling <598184085@qq.com>
2018-08-08 12:24:22 +0800

    add: 9-4

--- src/component/user/user.js
+++ src/component/user/user.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {Result, List,Brief,WhiteSpace,Modal} from 'antd-mobile'
-
+import browserCookie from 'browser-cookies'
+// import {logoutSubmit} from '../../redux/user.redux'
+import {Redirect} from 'react-router-dom'
 @connect(
 	state=>state.user,
 )
 class User extends React.Component{
+	constructor(props){
+		super(props)
+		this.logout = this.logout.bind(this)
+	}
+	logout(){
+		const alert = Modal.alert
+
+		alert('注销', '确认退出登录吗???', [
+		      { text: '取消', onPress: () => console.log('cancel') },
+		      { text: '确认', onPress: () => {
+		      	browserCookie.erase('userid')
+						window.location.href = window.location.href
+		      }}
+		    ])
+	}
 	render(){
 		const props = this.props
 		const Item = List.Item
@@ -16,23 +33,25 @@ class User extends React.Component{
 				<Result
 					img={<img src={require(`../img/${props.avatar}.png`)} style={{width:50}} alt="" />}
 					title={props.user}
-					message={props.type=='boss'? props.company: null}
+					message={props.type=='boss'?props.company:null}
 				/>
-			<List renderHeader={() => '简介'}>
-				<Item
-					multipleLine
-				>
-				{props.title}
-				{props.desc.split('\n').map(v=><Brief key={v}>{v}</Brief>)}
-				{props.money?<Brief>薪资:{props.money}</Brief>:null}
-				</Item>
-			</List>
-			<WhiteSpace></WhiteSpace>
-			<List>
-				<Item onClick={this.logout}>退出登录</Item>
-			</List>
+
+				<List renderHeader={()=>'简介'}>
+					<Item
+						multipleLine
+					>
+						{props.title}
+						{props.desc.split('\n').map(v=><Brief key={v}>{v}</Brief>)}
+						{props.money?<Brief>薪资:{props.money}</Brief>:null}
+					</Item>
+
+				</List>
+				<WhiteSpace></WhiteSpace>
+				<List>
+					<Item onClick={this.logout} style={{zIndex: 99}} >退出登录</Item>
+				</List>
 			</div>
-		):null
+		):<Redirect to={props.redirectTo} />
 
 	}
 }
